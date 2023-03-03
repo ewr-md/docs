088668809d2a615d587858efe44b747cbd950565
ewr <ewrobbins@outlook.com>
2023-03-03 15:00:23 -0500

    file folder structure change, fixed bibliographic output

---
+++ presentations/journalClub/reduce-trial/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='morning report journal club for REDUCE trial. Beamer presentation.',
+    author='Eric W. Robbins',
+    license='MIT',
+)
