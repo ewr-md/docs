53869b821c443e3ef4ed5102131c392896dfa85e
ewr <ewrobbins@outlook.com>
2025-04-29 20:37:43 -0400

    updated CV

---
+++ cv_resume/src/bold.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+input=./works.bib
+output=./personal_bib.bib
+
+rm $output
+cat $input | sed -e 's/Robbins, Eric William/\\textbf{Robbins, Eric William}/g' -e 's/Robbins, Eric W\./\\textbf{Robbins, Eric W\.}/g' -e 's/Robbins, Eric W/\\textbf{Robbins, Eric W}/g' -e 's/Robbins, Eric/\\textbf{Robbins, Eric}/g' -e 's/Robbins, E\./\\textbf{Robbins, E\.}/g' > $output
