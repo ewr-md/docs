47f76d494d667f6556034f10cf45821d8fa392d0
ewr-md <ewrobbins@outlook.com>
2025-05-09 17:57:02 -0400

    added pulm circ pub

--- cv_resume/src/bold.sh
+++ cv_resume/src/bold.sh
@@ -4,4 +4,4 @@ input=./works.bib
 output=./personal_bib.bib
 
 rm $output
-cat $input | sed -e 's/Robbins, Eric William/\\textbf{Robbins, Eric William}/g' -e 's/Robbins, Eric W\./\\textbf{Robbins, Eric W\.}/g' -e 's/Robbins, Eric W/\\textbf{Robbins, Eric W}/g' -e 's/Robbins, Eric/\\textbf{Robbins, Eric}/g' -e 's/Robbins, E\./\\textbf{Robbins, E\.}/g' > $output
+cat $input | sed -e 's/Robbins, Eric William /\\textbf{Robbins, Eric William} /g' -e 's/Robbins, Eric W\./\\textbf{Robbins, Eric W\.}/g' -e 's/Robbins, Eric W /\\textbf{Robbins, Eric W} /g' -e 's/Robbins, Eric and/\\textbf{Robbins, Eric} and/g' -e 's/Robbins, E\./\\textbf{Robbins, E\.}/g' >>$output
